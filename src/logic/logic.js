c7c2b53986a79c84f3f74338b19f28930b42bfba
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-05-16 19:01:18 +0300

    Added a feature to add a task to a project

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -7,13 +7,15 @@ export const allProjects = [];
 const firstTask = taskManager.createTask(
     "To Do Project", "To Doing The To Do", "2025-05-20", "Lowest", "Default"
 );
+
+const secondTask = taskManager.createTask(
+    "Not To Do", "To Not Doing The To Do", "2025-05-20", "Highest", "Default"
+)
+
 allTasks.push(firstTask);
 
-console.log(allTasks)
-
 const defaultProject = projectManager.createProject("Default");
-defaultProject.projectTasks.push(firstTask)
-console.log(defaultProject)
 
-projectManager.deleteTask("Some Random Project", defaultProject);           
-console.log(defaultProject);
+projectManager.addTask(defaultProject, firstTask, secondTask);
+
+console.log(defaultProject)
--- src/logic/project.js
+++ src/logic/project.js
@@ -5,6 +5,31 @@ const handleProjects = (function() {
         }
     }
 
+    function addTask(project, ...task) {
+        const tasks = project.projectTasks;
+        
+        if (task.length != 0) {
+            if (tasks.length != 0) {
+                for (let i = 0; i < tasks.length; i++) {
+                    for (let j = 0; j < task.length; j++) {
+                        if (tasks[i].title != task[j].title) {
+                            return tasks.push(task[j]);
+                        } else {
+                            return `${task[j].title} already exists`
+                        }
+                    }   
+                }
+            } else {
+                for (let m = 0; m < task.length; m++) {
+                   tasks.push(task[m]);
+                }
+                return tasks
+            }
+        } 
+
+        return tasks.push(task[0])
+    }
+
     function deleteTask(title, project) {
         const tasks = project.projectTasks
         if (tasks.length != 0) {
@@ -20,6 +45,7 @@ const handleProjects = (function() {
 
     return {
         createProject,
+        addTask,
         deleteTask
     }
 })
