77035485212f73a548a204855c48f3755b34e2f7
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-05-19 14:47:48 +0300

    Decided to start with projects

--- src/ui/mainUi.js
+++ src/ui/mainUi.js
@@ -10,12 +10,32 @@ function getButton() {
     return document.createElement("button");
 }
 
-export { getDiv, getButton }
+export { getDiv, getButton };
+
+function displayProjectModal() {
+    const projectModal = document.querySelector(".projectModal");
+    console.log(projectModal.style.display)
+    if (projectModal.style.display == "" || projectModal.style.display == "none") {
+        projectModal.style.display = "flex";
+        return;
+    } else if (projectModal.style.display == "flex") {
+        projectModal.style.display = "none";
+        return;
+    }
+}
 
 
-// document.getElementById("addToDoBtn").addEventListener(
-//     "click", displayTaskModal
-// )
+
+
+document.getElementById("addToDoBtn").addEventListener(
+    "click", () => {
+        // some code here;
+    }
+)
+
+document.getElementById("addProjectBtn").addEventListener(
+    "click", displayProjectModal
+)
 
 document.getElementById("allTasksTab").addEventListener(
     "click", contentManager.dipslayAllTasks
