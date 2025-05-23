61b805c6403cc2591b74f4c62deb57018af1d4b8
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-05-23 19:03:07 +0300

    Added a task manager and a project manager for the app logic

---
+++ src/logic/logic.js
@@ -0,0 +1,90 @@
+let allTasks = [
+    {title: "Hello", desc: "Not Hello really", due: "Idk", priority: "Hmm"}
+];
+let allProjects = []
+
+const taskManager = (function() {
+    function createTask(title, desc, due, priority) {
+        const newTask = {
+            title, desc, due, priority
+        }
+        if (allTasks.length > 0) {
+            const result = allTasks.filter((task) => task.title == newTask.title);
+            if (result.length == 0) {
+                allTasks.push(newTask);
+                return newTask;
+            } else {
+                return `${newTask.title} already exists`
+            }
+        }
+    }
+
+    function editTask(task, context, change) {
+        if (allTasks.includes(task)) {
+            return task[context] = change;
+        } else if (!allTasks.includes(task)) {
+            return `${task.title} does not exist`
+        }
+    }
+    return {
+        createTask,
+        editTask
+    }
+})();
+
+const projectManager = (function() {
+    function createProject(name) {
+        let project = {
+            name: "",
+            tasks: []
+        }
+        if (!allProjects.includes(project)) {
+            project.name = name;
+            allProjects.push(project)
+            return project;
+        } else {
+            return `${name} already exists`;
+        }
+    }
+
+    function addTask(project, task) {
+        if (allProjects.includes(project)) {
+            if (!project.tasks.includes(task)) {
+                project.tasks.push(task);
+                return;
+            } else {
+                return `${task.title} already exists in ${project.name}`;
+            }
+        }
+    }
+    
+    function deleteTask(task) {
+        if (allTasks.includes(task)) {
+            allTasks.splice(allTasks.indexOf(task), 1);
+            for (let p = 0; p < allProjects.length; p++) {
+                if (allProjects[p].tasks[p].title == task.title) {
+                    allProjects[p].tasks.splice(allProjects.indexOf(allProjects[p]), 1);
+                }
+            }
+        }
+    }
+
+    return {
+        createProject,
+        addTask,
+        deleteTask
+    }
+})();
+
+const anotherTask = taskManager.createTask("Not Hello", "This is really hello", "Not sure", "Hmmeo");
+taskManager.editTask(anotherTask, 'title', 'nothing');
+
+console.log(allTasks)
+
+const newProject = projectManager.createProject("Default");
+
+projectManager.addTask(newProject, anotherTask);
+
+projectManager.deleteTask(anotherTask);
+
+console.log(allProjects)
