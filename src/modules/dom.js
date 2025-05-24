e65e7160f371f1b3c84943183e799a5a00fbb58c
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-05-24 11:40:46 +0300

    Added a handler for task modal and validator

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,3 +1,38 @@
 import { allTasks, createTask } from "./todo";
 import { allProjects, createProject } from "./project";
 
+function displayTaskModal() {
+    let taskModal = document.querySelector(".task-modal");
+    if (taskModal.style.display == 'none' || taskModal.style.display == "") {
+        return taskModal.style.display = 'flex';
+    } else if (taskModal.style.display == 'flex') {
+        return taskModal.style.display = 'none';
+    }
+}
+
+function validateTaskForm() {
+    let taskModal = document.querySelector(".task-modal")
+    const taskForm = document.forms['task-form'];
+    const taskName = taskForm['task-name'].value;
+    const taskDue = taskForm['task-due'].value;
+    const taskPriority = taskForm['task-priority'].value;
+
+    if (taskName != "" && taskDue != "") {
+        createTask(
+            taskName, taskDue, taskPriority
+        )
+        taskModal.style.display = "none";
+    } else if (taskName == "" || taskDue == "") {
+        alert("Fill the form please")
+    }
+}
+
+const addTaskBtn = document.querySelector(".add-task");
+addTaskBtn.addEventListener("click", displayTaskModal);
+
+
+const submitTaskBtn = document.getElementById("submit-task");
+submitTaskBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    validateTaskForm()
+});
