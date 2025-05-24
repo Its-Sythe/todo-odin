be752debeeb21300b3602f96c87656316f25e5a8
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-05-24 11:06:49 +0300

    Modified task module to ensure a more simpler check is done for task existing and such

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -11,18 +11,11 @@ function createTask(title, due, priority) {
         }
     }
 
-    const taskTitles = [];
-    if (allTasks.length != 0) {
-        for (let t = 0; t < allTasks.length; t++) {
-            taskTitles.push(allTasks[t].title);
-        }
-    }
-
-    if (!taskTitles.includes(newTask.title)) {
+    if (!allTasks.some(tasks => tasks.title === newTask.title)) {
         allTasks.push(newTask);
         return newTask;
-    } else if (taskTitles.includes(newTask.title)) {
-        return `${newTask.title} already exists.`
+    } else {
+        return `${newTask.title} already exist`;
     }
 }
 
